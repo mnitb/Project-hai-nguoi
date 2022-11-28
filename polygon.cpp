03ee0dd6bff1b1548f5dee88c7cedd582bc4c8a2
mnitb <70020332+mnitb@users.noreply.github.com>
2022-11-28 10:49:59 +0700

    Update polygon.cpp

--- polygon.cpp
+++ polygon.cpp
@@ -16,9 +16,9 @@ void setSideOfPolygon(Polygon &a) {
     for (int i = 0; i < a.PolygonSides; i++) {
         cout << "Nhap chieu dai canh thu "<<i+1<<" cua da giac: ";
         cin >> a.length[i];
-        while (a.PolygonSides <= 2) {
-            cout << "So canh khong hop le.\n";
-            cin>>a.PolygonSides;
+        while (a.length[i] <= 0) {
+            cout << "Chieu dai canh khong hop le\n";
+            cin>>a.length[i];
         }
     }
 }
